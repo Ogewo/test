d16c6b7c1879134eb5f094ecd398537b67f2cb24
Ogewo <71373828+Ogewo@users.noreply.github.com>
2022-10-17 17:04:27 +0300

    Add files via upload

---
+++ glassmorphism.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="style.css">
+    <body>
+        <div class="container">
+            <div class="card">
+                <div class="content">
+                    <h2>01</h2>
+                    <h3>Card One</h3>
+                    <p>SOLUTION 2 : OPTIMISED SOLUTION
+                        An optimized approach to solving this problem is to find a candidate element A[i] find a chance
+                         where it could probably be the majority element, Afterwards, a traversal can be done on the array A,
+                          keeping count of the number of occurrences of that particular candidate element that was pre-determined.
+                           If the count is greater than n/2 then the candidate element can be returned as the majority, else, return 
+                           the value of choice to indicate that there is no majority element in that particular group.
+                        
+                    </p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>  <div class="card">
+            <div class="content">
+                <h2>02</h2>
+                <h3>Card Two</h3>
+                <p>SOLUTION 2 : OPTIMISED SOLUTION
+                    An optimized approach to solving this problem is to find a candidate element A[i] find a chance
+                     where it could probably be the majority element, Afterwards, a traversal can be done on the array A,
+                      keeping count of the number of occurrences of that particular candidate element that was pre-determined.
+                       If the count is greater than n/2 then the candidate element can be returned as the majority, else, return 
+                       the value of choice to indicate that there is no majority element in that particular group.
+                    
+                </p>
+                <a href="#">Read More</a>
+        </div>
+    </div>  <div class="card">
+        <div class="content">
+            <h2>03</h2>
+            <h3>Card Three</h3>
+            <p>SOLUTION 2 : OPTIMISED SOLUTION
+                An optimized approach to solving this problem is to find a candidate element A[i] find a chance
+                 where it could probably be the majority element, Afterwards, a traversal can be done on the array A,
+                  keeping count of the number of occurrences of that particular candidate element that was pre-determined.
+                   If the count is greater than n/2 then the candidate element can be returned as the majority, else, return 
+                   the value of choice to indicate that there is no majority element in that particular group.
+                
+            </p>
+            <a href="#">Read More</a>
+    </div>
+</div>
+
+
+    </div>
+
+    </body>
+</html>
